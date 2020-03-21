61564f4d9c218b9011e7c2f9d152f657d1cf8ef9
Mike <reach@sysdevmike.com>
2020-03-21 23:43:20 +0900

    Test horizontal list

---
+++ components/simple-horizontal-list.tsx
@@ -0,0 +1,71 @@
+import React, {useState} from 'react'
+import {
+  DragDropContext,
+  Droppable,
+  Draggable,
+  DropResult,
+} from 'react-beautiful-dnd'
+
+export interface Item {
+  id: string
+  content: string
+}
+
+export default function SimpleVerticalList() {
+  const makeItems = (count: number): Item[] =>
+    Array.from({length: count}, (v, k) => k).map((k) => ({
+      id: `item-${k}`,
+      content: `item ${k}`,
+    }))
+
+  const [items, setItems] = useState(makeItems(10))
+
+  const reorder = (list: Item[], startIndex: number, endIndex: number) => {
+    const result = Array.from(list)
+    const [removed] = result.splice(startIndex, 1)
+    result.splice(endIndex, 0, removed)
+
+    return result
+  }
+
+  const onDragEnd = (result: DropResult) => {
+    if (!result.destination) {
+      return
+    }
+
+    const updated = reorder(
+      items,
+      result.source.index,
+      result.destination.index,
+    )
+
+    setItems(updated)
+  }
+
+  return (
+    <DragDropContext onDragEnd={onDragEnd}>
+      <Droppable droppableId="droppable" direction="horizontal">
+        {(provided) => (
+          <div {...provided.droppableProps} ref={provided.innerRef}>
+            {items.map((item, index) => (
+              <Draggable key={item.id} draggableId={item.id} index={index}>
+                {(provided) => (
+                  <div
+                    ref={provided.innerRef}
+                    {...provided.draggableProps}
+                    {...provided.dragHandleProps}
+                    aria-label={`${index}`}
+                    data-testid="item"
+                  >
+                    {item.content}
+                  </div>
+                )}
+              </Draggable>
+            ))}
+            {provided.placeholder}
+          </div>
+        )}
+      </Droppable>
+    </DragDropContext>
+  )
+}
---
+++ tests/components/simple-horizontal-list.test.tsx
@@ -0,0 +1,43 @@
+import React from 'react'
+import SimpleHorizontalList from '../../components/simple-horizontal-list'
+import {render} from '../../src'
+
+it('drags an item in front of another', () => {
+  const {drag, getAllByTestId} = render(<SimpleHorizontalList />, {
+    dragDirection: 'horizontal',
+  })
+
+  let first = getAllByTestId(/item/i)[0]
+  let second = getAllByTestId(/item/i)[1]
+
+  drag(second).inFrontOf(first)
+  first = getAllByTestId(/item/i)[0]
+  expect(first.textContent).toBe(second.textContent)
+
+  second = getAllByTestId(/item/i)[1]
+  const fourth = getAllByTestId(/item/i)[3]
+
+  drag(fourth).inFrontOf(second)
+  second = getAllByTestId(/item/i)[1]
+  expect(second.textContent).toBe(fourth.textContent)
+})
+
+it('drags an item behind another', () => {
+  const {drag, getAllByTestId} = render(<SimpleHorizontalList />, {
+    dragDirection: 'horizontal',
+  })
+
+  let first = getAllByTestId(/item/i)[0]
+  const second = getAllByTestId(/item/i)[1]
+
+  drag(first).behind(second)
+  first = getAllByTestId(/item/i)[0]
+  expect(first.textContent).toBe(second.textContent)
+
+  const sixth = getAllByTestId(/item/i)[5]
+  let ninth = getAllByTestId(/item/i)[8]
+
+  drag(sixth).behind(ninth)
+  ninth = getAllByTestId(/item/i)[8]
+  expect(ninth.textContent).toBe(sixth.textContent)
+})
